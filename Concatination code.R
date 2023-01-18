af7c6dea7aae7e3e4e653d5e9ed64d6f0d9a6e58
tonybaloney-code <100958810+tonybaloney-code@users.noreply.github.com>
2023-01-17 18:42:17 -0600

    Add files via upload

---
+++ Concatination code.R
@@ -0,0 +1,42 @@
+library("phylotools")
+library("tidyverse")
+
+##Code Testing
+
+vac_dnapol<- read.fasta(file.choose())
+vac_rnapol <- read.fasta(file.choose())
+## sequences imported as strings
+
+##Have to clean sequence names
+vac1<- separate(vac_dnapol, "seq.name",
+                              into = "name",
+                              sep = (".CDS."),
+                              extra = "drop")%>%
+  separate(., "name",
+           into = "accession",
+           sep = (".peg."),
+           extra = "drop")%>%
+  mutate(., name = "vac")%>%
+  unite(., "name", accession, name, sep = "") ##WORKED!!!!!
+
+
+##concatenation
+vac1 <- namecleaner(vac_dnapol, "vaccinia")
+
+
+dat2fasta() # exports sequence into fasta file
+
+##Finalize##
+namecleaner <- function(sequence, species) {
+  separate(sequence, "seq.name",
+           into = "name",
+           sep = (".CDS."),
+           extra = "drop")%>%
+    separate(., "name",
+             into = "accession",
+             sep = (".peg."),
+             extra = "drop")%>%
+    mutate(., name = species)%>%
+    unite(., "name", accession, name, sep = "")
+}
+Concfasta <- function(sequence_list, species)
