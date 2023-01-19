cc76cd25c2c8f14aa2f08ff9dfed19294d626478
tonybaloney-code <tonyspam333@gmail.com>
2023-01-18 21:14:26 -0600

    Update Concatination code.R

--- Concatination code.R
+++ Concatination code.R
@@ -1,17 +1,47 @@
 library("phylotools")
 library("tidyverse")
 
+prot1 <- read.fasta(file.choose())
+prot2 <- read.fasta(file.choose())
+prot3 <- read.fasta(file.choose())
+species = "vaccinia" #Type in species here
+concfasta <- function(prot1, prot2, prot3, species){
+  namecleaner <- function(sequence, species) {
+    separate(sequence, "seq.name",
+             into = "seq.name",
+             sep = (".CDS."),
+             extra = "drop")%>%
+      separate(., "seq.name",
+               into = "accession",
+               sep = (".peg."),
+               extra = "drop")%>%
+      mutate(., name = species)%>%
+      unite(., "seq.name", accession, name, sep = "")
+  }
+  prot1 <- namecleaner(prot1, species)
+  prot2 <- namecleaner(prot2, species)
+  prot3 <- namecleaner(prot3, species)
+  conc <-  left_join(prot2,prot1, by ="seq.name")%>%
+    left_join(., prot3, by = "seq.name")%>%
+    unite(., "seq.text", seq.text, seq.text.x, seq.text.y, sep = "")
+  dat2fasta(conc)
+}
+concfasta(prot1,prot2,prot3, species)
+
+
 ##Code Testing
 
-vac_dnapol<- read.fasta(file.choose())
-vac_rnapol <- read.fasta(file.choose())
+prot1 <- read.fasta(file.choose())
+prot2 <- read.fasta(file.choose())
+prot3 <- read.fasta(file.choose())
+species = "vaccinia" #Type in species here
 ## sequences imported as strings
 
-##Have to clean sequence names
+##Name cleaning testing
 vac1<- separate(vac_dnapol, "seq.name",
-                              into = "name",
-                              sep = (".CDS."),
-                              extra = "drop")%>%
+                into = "name",
+                sep = (".CDS."),
+                extra = "drop")%>%
   separate(., "name",
            into = "accession",
            sep = (".peg."),
@@ -19,24 +49,48 @@ vac1<- separate(vac_dnapol, "seq.name",
   mutate(., name = "vac")%>%
   unite(., "name", accession, name, sep = "")
 
-
-##concatenation
-vac1 <- namecleaner(vac_dnapol, "vaccinia")
-
+#Concatenation testing
+prot1 <- namecleaner(prot1, species)
+prot2 <- namecleaner(prot2, species)
+prot3 <- namecleaner(prot3, species)
+conc <-  left_join(prot2,prot1, by ="seq.name")%>%
+  left_join(., prot3, by ="seq.name")%>%
+  unite(conc, "seq.text", seq.text, seq.text.x, seq.text.y, sep = "")
 
 dat2fasta() # exports sequence into fasta file
 
 ##Finalize##
 namecleaner <- function(sequence, species) {
   separate(sequence, "seq.name",
-           into = "name",
+           into = "seq.name",
            sep = (".CDS."),
            extra = "drop")%>%
-    separate(., "name",
+    separate(., "seq.name",
              into = "accession",
              sep = (".peg."),
              extra = "drop")%>%
     mutate(., name = species)%>%
-    unite(., "name", accession, name, sep = "")
+    unite(., "seq.name", accession, name, sep = "")
 }
-Concfasta <- function(sequence_list, species)
+concfasta <- function(prot1, prot2, prot3, species){
+  namecleaner <- function(sequence, species) {
+    separate(sequence, "seq.name",
+             into = "seq.name",
+             sep = (".CDS."),
+             extra = "drop")%>%
+      separate(., "seq.name",
+               into = "accession",
+               sep = (".peg."),
+               extra = "drop")%>%
+      mutate(., name = species)%>%
+      unite(., "seq.name", accession, name, sep = "")
+  }
+  prot1 <- namecleaner(prot1, species)
+  prot2 <- namecleaner(prot2, species)
+  prot3 <- namecleaner(prot3, species)
+  conc <-  left_join(prot2,prot1, by ="seq.name")%>%
+    left_join(., prot3, by = "seq.name")%>%
+    unite(., "seq.text", seq.text, seq.text.x, seq.text.y, sep = "")
+  dat2fasta(conc)
+}
+concfasta(prot1,prot2,prot3, species)
